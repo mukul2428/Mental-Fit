faeeb8eb6bb676b74ee2e208024a68ce46fec435
Gaurang Mudgal <gaurangmudgal13@gmail.com>
2022-05-03 12:50:46 +0530

    Add topic fix

--- routes/pages/room/addtopic.js
+++ routes/pages/room/addtopic.js
@@ -6,13 +6,13 @@ const CDN = '';
 module.exports = (req, res) => {
 	const slug = req.params.slug;
 
-  let config = {
-    cdn: CDN,
-    room: slug,
-    pageTitle: undefined
-  }
+	let config = {
+		cdn: CDN,
+		room: slug,
+		pageTitle: undefined
+	}
 
-	if(req.session == null || req.session.user == null) {
+	if (req.session == null || req.session.user == null) {
 		//redirect if not logged in
 		res.redirect('/');
 	} else {
@@ -21,30 +21,33 @@ module.exports = (req, res) => {
 			.then(data => {
 				config['user'] = data;
 
-				return Room.find({subscribers: data.id}); //get all rooms
+				return Room.find({ subscribers: data.id }); //get all rooms
 			})
 			.then(rooms => {
 				config['rooms'] = rooms;
 
 				// get the room that is being fetched on the page params
-	      return Room.find({
-	        slug: slug
-	      })
-	    })
-	    .then(rooms => {
-	      if(rooms.length > 0) {
-	        //if found within the database, render the room
-
-					config['pageTitle'] = rooms[0].category + ' > Add A Topic';
-
-	        res.render('addtopic', config)
-	      } else if(rooms.length === 0) {
-	        //if there are no results, tell client the room was not found
-	        res.json({
-	          confirmation: 'fail',
-	          data: 'Room not found: ' + slug
-	        })
-	      }
+				return Room.find({
+					slug: slug
+				})
+			})
+			.then(rooms => {
+				if (rooms.length > 0) {
+					//if found within the database, render the room
+					if (rooms[0].subscribers.includes(User.findById(req.session.user.id))) {
+						config['pageTitle'] = rooms[0].category + ' > Add A Topic';
+						res.render('addtopic', config)
+					} else {
+						let redirectURL = '/room/' + rooms[0].category.toLowerCase()
+						res.redirect(redirectURL)
+					}
+				} else if (rooms.length === 0) {
+					//if there are no results, tell client the room was not found
+					res.json({
+						confirmation: 'fail',
+						data: 'Room not found: ' + slug
+					})
+				}
 			})
 			.catch(err => {
 				res.render('addtopic', config);
